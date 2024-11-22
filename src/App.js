2a07c55e410f897e97432321b778c0ea7d8f9b0f
E-Rick-M <rickomikey@gmail.com>
2024-11-22 20:41:13 +0300

    Added Dummy Tasks with Custom Hooks

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react';
+
+import Tasks from './components/Tasks/Tasks';
+import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/use-http';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+
+
+
+  //   const transformTasks =useCallback((tasksObj) => {
+  //     const loadedTasks = [];
+
+  //     for (const taskKey in tasksObj) {
+  //       loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+  //     }
+
+  //     setTasks(loadedTasks);
+  //   },[]) ;
+
+  //   const { isLoading, error, sendRequest: fetchTasks } = useHttp(transformTasks);
+
+  //   useEffect(() => {
+  //   fetchTasks(
+  //     { url: 'https://react-http-tasks-9ae8c-default-rtdb.firebaseio.com/tasks.json' }
+  //   );
+  // }, [fetchTasks]);
+
+
+
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
+
+  useEffect(() => {
+    const transformTasks =((tasksObj) => {
+      const loadedTasks = [];
+  
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+  
+      setTasks(loadedTasks);
+    }) ;
+  fetchTasks(
+    { url: 'https://react-http-tasks-9ae8c-default-rtdb.firebaseio.com/tasks.json' },transformTasks
+  );
+}, [fetchTasks]);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
